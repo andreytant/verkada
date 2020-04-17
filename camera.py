885544ae51a68267aa559468bc91570694ce6a24
Andriy Tantsuyev <starco@Andriys-MacBook-Pro.local>
2020-04-17 12:45:32 -0700

    add camera as script, add log return

--- camera.py
+++ camera.py
@@ -1,13 +1,10 @@
-from flask import Flask, request
-import threading
-app = Flask(__name__)
-
-@app.route('/', methods=['GET'])
-def hello_world():
-    return 'Hi, this is server!'
-
-@app.route('/getMessage', methods=['GET'])
-def getMessage():
-    return "To be completed"
-
-app.run(debug=True, port=4000)
+import requests
+# wait timeout for a message from client
+timeout = 5
+GET_MSG_URL = "http://127.0.0.1:5000/getMessage"
+POST_MSG_URL = "http://127.0.0.1:5000/postLogs"
+r = requests.get(url = GET_MSG_URL, params = {'timeout':timeout})
+print ("Camera received message from client: {}".format(r.text))
+# log message which client will receive from camera
+logs = "log from camera is: " + r.text
+p = requests.post(url = POST_MSG_URL, params = {'logs':logs})
--- server.py
+++ server.py
@@ -7,17 +7,23 @@ app = Flask(__name__)
 class ServerContext:
     def __init__(self):
         self.msg = None
-
+        self.logs = None
     def receive_msg_from_client(self, msg):
         self.msg = msg
-
     def send_mgs_to_camera(self):
         self.msg = None
-
     def get_msg(self):
         return self.msg
+    def receive_logs_from_camera(self, logs):
+        self.logs = logs
+    def send_logs_to_client(self):
+        self.logs = None
+    def get_logs(self):
+        return self.logs
 
+LOG_WAIT_TIMEOUT = 5
 msg_available = threading.Event()
+logs_available = threading.Event()
 server_context = ServerContext()
 
 @app.route('/', methods=['GET'])
@@ -29,8 +35,8 @@ def hello_world():
 def getMessage():
     timeout = request.args.get('timeout',default = 2)
     msg_available.wait(int(timeout))
-    msg_available.clear()
     msg = server_context.get_msg()
+    msg_available.clear()
     server_context.send_mgs_to_camera()
     if msg is None:
         return 'Timeout. No message received'
@@ -38,12 +44,24 @@ def getMessage():
         return msg
 
 #Client/User sends the message
-@app.route('/sendMessage', methods=['GET'])
+@app.route('/sendMessage', methods=['POST'])
 def sendMessage():
     msg = request.args.get('message')
     server_context.receive_msg_from_client(msg)
     msg_available.set()
-    return 'Server received the message: {}'.format(msg)
+    #Waiting for logs to be sent from camera
+    logs_available.wait(LOG_WAIT_TIMEOUT)
+    logs = server_context.get_logs()
+    logs_available.clear()
+    server_context.send_logs_to_client()
+    return 'Server received logs from camera: {}'.format(logs)
+
+@app.route('/postLogs', methods=['POST'])
+def postLogs():
+    logs = request.args.get('logs')
+    server_context.receive_logs_from_camera(logs)
+    logs_available.set()
+    return 'Server received the message: {}'.format(logs)
 
 # this will run the server on port 5000, give it a try!
 app.run(debug=True, port=5000)
